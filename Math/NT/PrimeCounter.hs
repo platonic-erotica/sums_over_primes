bcd1f0c8641e0cdadaec4e71524768ca8024f46e
Gideon Providence <g@math.utoronto.ca>
2016-07-21 20:44:34 -0400

    missed a few additions

--- Math/NT/PrimeCounter.hs
+++ Math/NT/PrimeCounter.hs
@@ -1,9 +1,15 @@
-binaryFilter_Chebotarev :: [Int] -> 
+module Math.NT.PrimeCounter where
+
+sumChebotarev' :: [Int] -> Int
+sumChebotarev' primes = sum binaryFilter_Chebotarev' primes
+
+binaryFilter_Chebotarev' :: [Int] -> [Int]
+binaryFilter_Chebotarev' primes = map mappableFilter_Chebotarev primes
 
 mappableFilter_Chebotarev :: Int -> Int -> Int
 mappableFilter_Chebotarev p q
   | pEq1_modq p q && nuEq1_modp p q = 1
-  | otherwise                      = 0
+  | otherwise                       = 0
 
 pEq1_modq :: Int -> Int -> Bool
 pEq1_modq p q = (p `mod` q) == 1
@@ -16,4 +22,3 @@ nuEq1_modp p q
 
 pM1Eq0_modq :: Int -> Int -> Bool
 pM1Eq0_modq p q = ((p - 1) `mod` q) == 1
-
